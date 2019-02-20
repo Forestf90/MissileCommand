f362bae763ee5490dab96cac1a238da89cf5d5c2
Forestf <michalsliwa96@wp.pl>
2019-02-20 16:14:35 +0100

    warunki przegranej , pozycje

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ import math
 width = 480
 height = 480
 level=0
-pozycje = [35 ,90, 130 ,170 ,210 ,240, 445]
+pozycje = [35 ,90, 140,190 ,240,290,340,390, 445]
 pociski_wroga=[]
 punkty=0
 pociski=[]
 wybuchy=[]
 domy =[True , True, True, True , True , True]
+#domy=[False,False,False,False,False,False]
 stanowiska =[stanowisko(0), stanowisko(1), stanowisko(2)]
 stanowiska_poz = [30 , width/2 , width-30]
 kolory =[pygame.Color(0,255,0),pygame.Color(255,0,0), pygame.Color(255,255,0),
@@ -26,19 +27,9 @@ s=pygame.display.set_mode((height, width));
 pygame.display.set_caption('Missile Command');
 clock = pygame.time.Clock()
 
-rakieta_img =pygame.Surface((3,4))
+
 pygame.mouse.set_cursor(*pygame.cursors.diamond)
-def rysuj_rakieta():
-    kol = pygame.Color(0,0,255)
-    rakieta_img.fill((255,255,0))
-    rakieta_img.set_at((1, 0), kol)
-    rakieta_img.set_at((1, 1), kol)
-    rakieta_img.set_at((1, 2), kol)
-    rakieta_img.set_at((1, 3), kol)
-    rakieta_img.set_at((0, 3), kol)
-    rakieta_img.set_at((2, 3), kol)
-    rakieta_img.set_at((0, 4), kol)
-    rakieta_img.set_at((2, 4), kol)
+
 def rysuj_mape():
     s.fill((0,0,0))
     w =height -50
@@ -59,7 +50,7 @@ def rysuj_mape():
             poziom= poziom+1
     trzy=1
     for i in range(len(domy)):
-        pygame.draw.rect(s,pygame.Color(0,0,255),((i+trzy)*50 +30,height-40,20,20))
+        if(domy[i]==True):pygame.draw.rect(s,pygame.Color(0,0,255),((i+trzy)*50 +30,height-40,20,20))
         if((i+1)%3==0):
             trzy=trzy+1
         
@@ -168,6 +159,25 @@ def kolizje():
         if p.aktualnyy-p.koniecy>-0.1:
             temp = wybuch(p.aktualnyx, p.aktualnyy)
             wybuchy.append(temp)
+            if p.koniecx==35:
+                stanowiska[0].amunicja=0
+            elif p.koniecx==90:
+                domy[0]=False
+            elif p.koniecx==140:
+                domy[1]=False
+            elif p.koniecx==190:
+                domy[2]=False
+            elif p.koniecx==240:
+                stanowiska[1].amunicja=0
+            elif p.koniecx==290:
+                domy[3]=False
+            elif p.koniecx==340:
+                domy[4]=False
+            elif p.koniecx==390:
+                domy[5]=False
+            elif p.koniecx==445:
+                stanowiska[2].amunicja=0
+            przegrana()
             pociski_wroga.remove(p)
             del p
             continue
@@ -188,7 +198,43 @@ def level():
             temp = rakieta(random.randrange(width) ,0
             , random.choice(pozycje) ,height-30 , 0.04, random.randrange(4000))
             pociski_wroga.append(temp)
+def przegrana():
+    for i in domy:
+        if i==True:
+            return
+    del wybuchy[:]
+    del pociski[:]
+    del pociski_wroga[:]
+    rysuj_mape()
+    largeText = pygame.font.SysFont("consolas",12)
+    textSurface = largeText.render("Uzyskane punkty : "+str(punkty)+
+                                   " Wciśnij spacje aby zacząć od nowa", True, (255,0,0))
+    TextRect= textSurface.get_rect()
+    TextRect.center = ((width/2),(height/2))
+    s.blit(textSurface, TextRect)
     
+    pygame.display.update()
+    global punkty
+    while True:
+        for event in pygame.event.get():
+            #print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                        stanowiska[0].amunicja=10
+                        stanowiska[1].amunicja=10
+                        stanowiska[2].amunicja=10
+                        domy[0]=True
+                        domy[1]=True
+                        domy[2]=True
+                        domy[3]=True
+                        domy[4]=True
+                        domy[5]=True
+                        punkty=0
+                        main()
+                        
 def main():
      while True:
         kolizje()
@@ -202,5 +248,4 @@ def main():
                     strzal(x,y)
      clock.tick(60)
 
-rysuj_rakieta()
 main()
